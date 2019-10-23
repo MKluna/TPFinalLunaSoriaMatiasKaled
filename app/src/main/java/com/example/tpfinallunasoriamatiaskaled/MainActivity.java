1888d7d04756821bd763bdc2fd5dfdbe5b94d8b6
Matias <mkluna@institutosanmartin.edu.ar>
2019-10-23 20:25:55 -0300

    prueba 20:25 23/10/2019

--- app/src/main/java/com/example/tpfinallunasoriamatiaskaled/MainActivity.java
+++ app/src/main/java/com/example/tpfinallunasoriamatiaskaled/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.tpfinallunasoriamatiaskaled;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button botonprueba;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        botonprueba = findViewById(R.id.botonprueba);
+        botonprueba.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
